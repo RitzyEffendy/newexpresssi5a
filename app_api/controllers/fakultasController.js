0ca2f38728337f203bd3edf9911fd795580eac51
RitzyEffendy <ritzyeffendy_2327240028@mhs.mdp.ac.id>
2025-10-24 15:14:57 +0700

    24 Oktober 2025

--- app_api/controllers/fakultasController.js
+++ app_api/controllers/fakultasController.js
@@ -12,6 +12,40 @@ const getAllFakultas = async (req, res) => {
     }
 }
 
+// Fungsi untuk mengambil isi collection fakultas berdasarkan parameter ID
+const getFakultasById = async (req, res) => {
+    try {
+        // GET collection fakultas parameter ID
+        const result = await fakultasSchema.findById(req.params.id)
+        if(!result){
+            // Jika ada fakultas tidak ada MongoDB
+            res.status(404).json({ message: "Fakultas tidak ditemukan"})
+        } else {
+            // Jika data fakultas ada
+            res.status(200).json(result)
+        }
+    } catch (error) {
+        res.status(500).json({message: error.message})
+    }
+}
+
+const deleteFakultasById = async (req, res) => {
+    try {
+        // GET collection fakultas parameter ID
+        const result = await fakultasSchema.findById(req.params.id)
+        if(!result){
+            // Jika ada fakultas tidak ada di MongoDB
+            res.status(404).json({ message: "Fakultas tidak ditemukan"})
+        } else {
+            // Jika data fakultas ada, maka hapus data fakultas berdasarkan Id
+            await result.deleteOne();
+            res.status(200).json({message: "Fakultas berhasil dihapus"})
+        }
+    } catch (error) {
+        res.status(500).json({message: error.message})
+    }
+}
+
 const createFakultas = async (req, res) => {
     // Buat instance fakultas baru
     const fakultas = new fakultasSchema({
@@ -25,5 +59,31 @@ const createFakultas = async (req, res) => {
     res.status(201).json(hasil);
 }
 
+
+
+const updateFakultasById = async (req, res) => {
+    try {
+        // GET collection fakultas parameter ID
+        const result = await fakultasSchema.findById(req.params.id)
+        if(!result){ // Jika ada fakultas tidak ada MongoDB
+            res.status(404).json({ message: "Fakultas tidak ditemukan"})
+        } else { // Jika data fakultas ada
+            // Jika ada request perubahan nama
+            if(req.body.nama != null){
+                result.nama = req.body.nama
+            }
+            // Jika ada request perubahan singkatan
+            if(req.body.singkatan != null){
+                result.singkatan = req.body.singkatan
+            }
+            // Update data fakultas
+            const updateFakultas = await result.save()
+            res.status(200).json(updateFakultas)
+        }
+    } catch (error) {
+        res.status(500).json({message: error.message})
+    }
+}
+
 // Export
-module.exports = {getAllFakultas, createFakultas}
+module.exports = {getAllFakultas, getFakultasById, createFakultas, deleteFakultasById, updateFakultasById}
--- app_api/routes/fakultas.js
+++ app_api/routes/fakultas.js
@@ -7,6 +7,9 @@ const fakultasController = require("../controllers/fakultasController")
 // Route GET fakultas
 router.get("/", fakultasController.getAllFakultas)
 router.post("/", fakultasController.createFakultas)
+router.get("/:id", fakultasController.getFakultasById)
+router.delete("/:id", fakultasController.deleteFakultasById)
+router.put("/:id", fakultasController.updateFakultasById)
 
 // Export module
 module.exports = router;
